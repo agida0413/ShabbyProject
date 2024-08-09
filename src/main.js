4f32817c7270f2c13ef2d1f4e6de4d6cb8f48a21
Your Name <agida0413@naver.com>
2024-08-09 17:18:49 +0900

    On branch master   Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	modified:   src/main.js 	new file:   src/router/index.js 	new file:   src/views/AboutView.vue 	new file:   src/views/HomeView.vue
     Changes not staged for commit:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/index.html
    	modified:   src/App.vue
    	modified:   src/components/HelloWorld.vue
    	deleted:    src/router/index.js
    	modified:   src/views/HomeView.vue
    	modified:   vue.config.js
    
     Untracked files:
    	src/assets/background.jpg
    	src/assets/logo.svg
    	src/assets/logo2.png
    	src/components/common/
    	src/components/feed/
    	src/components/member/
    	src/plugins/
    	src/router/router.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import Footer from './components/common/Footer.vue'
+import Header from './components/common/Header.vue'
+loadFonts()
 
-createApp(App).mount('#app')
+createApp(App)
+.component('Footer',Footer)
+.component('Header',Header)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
