7e354f1458261ba3b7e320955200227ea4d9fcae
Your Name <agida0413@naver.com>
2024-08-14 09:58:31 +0900

    init

---
+++ ShabbyProject_Api_Server/src/main/java/com/sist/ShabbyProjectApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.sist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShabbyProjectApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShabbyProjectApiServerApplication.class, args);
+	}
+
+}
