2f2fce2ee396405eb7fe1841645aed8cd65ed769
agida0413 <144654789+agida0413@users.noreply.github.com>
2024-08-21 23:37:07 +0900

    123

--- ShabbyProject_Api_Server/src/main/java/com/sist/common/exception/BadRequestException.java
+++ ShabbyProject_Api_Server/src/main/java/com/sist/common/exception/BadRequestException.java
@@ -6,8 +6,8 @@ public class BadRequestException extends RuntimeException{
 	
 		private final String errorCode;
 		private final String errorMessage;
+	
 	    //400 에러 관련 사용자 정의 예외
-
 		public BadRequestException(String errorMessage) {//클라이언트에서 받을 메시지 
 			// TODO Auto-generated constructor stub
 			super(errorMessage);
