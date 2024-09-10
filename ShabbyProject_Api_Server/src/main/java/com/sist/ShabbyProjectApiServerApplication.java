a3b3e8f82ea28b491b4c859e468e710034078864
agida0413 <144654789+agida0413@users.noreply.github.com>
2024-09-10 22:36:42 +0900

    res

--- ShabbyProject_Api_Server/src/main/java/com/sist/ShabbyProjectApiServerApplication.java
+++ ShabbyProject_Api_Server/src/main/java/com/sist/ShabbyProjectApiServerApplication.java
@@ -3,6 +3,9 @@ package com.sist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+
 @SpringBootApplication
 public class ShabbyProjectApiServerApplication {
 
