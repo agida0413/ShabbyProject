625ce3d5a35ff21b4cc54772bbbe06db26c1dccb
KimYJInComapny <agida0413>
2024-09-16 18:32:53 +0900

    s

--- Front_Vue/vue.config.js
+++ Front_Vue/vue.config.js
@@ -1,16 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  devServer: {
+  // devServer: {
     
-    proxy: {
-      '/api': { 
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        ws:false,
+  //   // proxy: {
+  //   //   '/api': { 
+  //   //     target: 'http://localhost:8080',
+  //   //     changeOrigin: true,
+  //   //     ws:false,
         
-      }, 
-    },
-  },
+  //   //   }, 
+  //   // },
+  // },
+  outputDir:'C:/Users/TA9/git/termPj/ShabbyProject_Api_Server/src/main/resources/static',
   transpileDependencies: true,
   pluginOptions: {
     vuetify: {
